7c2a5648f96e8c09cf6e71d82c14cdd247773bab
Paul Water <p.r.water@setbased.nl>
2020-05-09 08:32:48 +0200

    Implemented methods getAcceptLanguage(), getReferrer(), and getRequestTime().

--- src/CoreRequest.php
+++ src/CoreRequest.php
@@ -18,6 +18,20 @@ class CoreRequest implements Request
 {
   //--------------------------------------------------------------------------------------------------------------------
   /**
+   * Returns the accepted languages by the user agent.
+   *
+   * @return string|null
+   *
+   * @api
+   * @since 1.0.0
+   */
+  public function getAcceptLanguage(): ?string
+  {
+    return $_SERVER['HTTP_ACCEPT_LANGUAGE'] ?? null;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
    * Returns the method of the current request.
    *
    * @return string
@@ -42,6 +56,21 @@ class CoreRequest implements Request
 
   //--------------------------------------------------------------------------------------------------------------------
   /**
+   * Returns the URL of the page (if any) which referred the user agent to the current page. This is set by the user
+   * agent and cannot be trusted.
+   *
+   * @return string|null
+   *
+   * @api
+   * @since 1.0.0
+   */
+  public function getReferrer(): ?string
+  {
+    return $_SERVER['HTTP_REFERER'] ?? null;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
    * Returns the remote IP (this is always the next hop, not necessarily the user's IP address).
    *
    * @return string|null
@@ -52,6 +81,20 @@ class CoreRequest implements Request
   public function getRemoteIp(): ?string
   {
     return $_SERVER['REMOTE_ADDR'] ?? null;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Returns the timestamp of the start of the request, with microsecond precision.
+   *
+   * @return float|null
+   *
+   * @api
+   * @since 1.0.0
+   */
+  public function getRequestTime(): ?float
+  {
+    return $_SERVER['REQUEST_TIME_FLOAT'] ?? null;
   }
 
   //--------------------------------------------------------------------------------------------------------------------
